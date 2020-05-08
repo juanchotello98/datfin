b5aaf849029582c51796281cbfa00684eb54716d
Juan David Tello <tello.juan@correounivalle.edu.co>
2020-05-08 00:55:06 -0500

    message

--- datfin/settings.py
+++ datfin/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-pmhnbt@_fnjhv)y8*#193@=w85tt+$r9@%ap(&@)od4y5f+@8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 #DEBUG = True
 
 ALLOWED_HOSTS = ['*']
