87603237f586076e8aa840a356008e5639992dc0
Juan David Tello <tello.juan@correounivalle.edu.co>
2020-05-08 00:34:26 -0500

    m

--- datfin/urls.py
+++ datfin/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url , include
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
-from django.config.urls.static import static
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
